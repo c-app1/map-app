f9270b9cfa30eed12c09d8fa32a5073d3015d13d
Tomohiro-cs11 <tomohiro@cs11.cs.kobe-u.ac.jp>
2017-07-17 19:28:33 +0900

    Initial commit

--- create_Address.js
+++ create_Address.js
@@ -31,20 +31,6 @@ counter = 0;
 //イメージアイコン
 var image_icon = ["marker_blue.png","marker_green.png","marker_purple.png"];
 
-//コンパス用
-var compass_canvas;
-var ctx;
-var compass_image;
-
-/*var directionsErr = new Array(); //ルート結果のエラーメッセージ 
-directionsErr[ds.INVALID_REQUEST] = "指定された DirectionsRequest が無効です。"; 
-directionsErr[ds.MAX_WAYPOINTS_EXCEEDED] = "DirectionsRequest に指定された DirectionsWaypoint が多すぎます。ウェイポイントの最大許容数は 8 に出発地点と到着地点を加えた数です。"; 
-directionsErr[ds.NOT_FOUND] = "出発地点、到着地点、ウェイポイントのうち、少なくとも 1 つがジオコード化できませんでした。"; 
-directionsErr[ds.OVER_QUERY_LIMIT] = "ウェブページは、短期間にリクエストの制限回数を超えました。"; 
-directionsErr[ds.REQUEST_DENIED] = "ウェブページではルート サービスを使用できません。"; 
-directionsErr[ds.UNKNOWN_ERROR] = "サーバー エラーのため、ルート リクエストを処理できませんでした。もう一度試すと正常に処理される可能性があります。"; 
-directionsErr[ds.ZERO_RESULTS] = "出発地点と到着地点間でルートを見つけられませんでした。"; 
-*/
 
 <!-- initialize()関数を定義 -->
 function initialize() {
@@ -56,18 +42,6 @@ function initialize() {
     timeout:10000, //タイムアウトは10秒
     maximumAge:0 //キャッシュは使用禁止
   };
-  
-  //コンパスの処理
-  compass_canvas = document.getElementById("compass_canvas");
-  ctx = compass_canvas.getContext("2d");
-  //コンパスの画像
-  var compass = "./compass.png";
-  
-  compass_image = new Image();
-  image.onload = function() {};
-  compass_image.src = compass;
-  update_compass(20);
-  
   
   // 現在の位置情報取得を実施 正常に位置情報が取得できると、
   // successCallbackがコールバックされる。
@@ -82,21 +56,18 @@ function initialize() {
 function successCallback(pos) {
   var Potition_latitude = pos.coords.latitude;
   var Potition_longitude = pos.coords.longitude;
-  var angle = pos.coords.heading;
   // 位置情報が取得出来たらGoogle Mapを表示する
-  start(Potition_latitude,Potition_longitude,angle);
+  start(Potition_latitude,Potition_longitude);
 }
  
 function errorCallback(error) {
   alert("位置情報が許可されていません。位置情報をオンにしてください。");
 }
 
-function start(x,y,angle){
+function start(x,y){
   
   myLatlng = new google.maps.LatLng(x,y);
-  if(angle){
-    update_compass(angle);
-  }
+  
   if (marker){// マーカーがすでにあるなら消去
     marker.setMap(null);
   }else{ //もし、初期化で呼び出されたならば
@@ -137,18 +108,6 @@ function get_area_name(latLng_now){
     }
   });
 }
-
-
-function update_compass(angle) {
-  var theta = angle / 180 * Math.PI;
-  ctx.clearRect(0, 0, compass_canvas.width, compass_canvas.height);
-  ctx.save();
-  ctx.translate(compass_canvas.width / 2, compass_canvas.height / 2);
-  ctx.rotate(theta);
-  ctx.drawImage(compass_image, -compass_image.width/2, -compass_image.height/2);
-  ctx.restore();
-}
-
 
 function search_route(){
   counter = 0;
@@ -194,25 +153,11 @@ function search_route(){
   directionsErr[ds.UNKNOWN_ERROR] = "サーバー エラーのため、ルート リクエストを処理できませんでした。もう一度試すと正常に処理される可能性があります。"; 
   directionsErr[ds.ZERO_RESULTS] = "出発地点と到着地点間でルートを見つけられませんでした。"; 
   
-  
-  /*myLatlng = new google.maps.LatLng(35.681382,139.766084);
-  
-  var mapOptions = {
-      center: myLatlng,
-      zoom: 50,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      //position: google.maps.ControlPosition.TOP_CENTER
-  };
-
-  // Mapオブジェクトに地図表示要素情報とオプション情報を渡し、インスタンス生成
-  map1 = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  */
-  
   var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions); // ルート案内
   directionsDisplay.setMap(map1);
   if (initialize_route_flag == true){
-    document.getElementById("route").removeChild(document.getElementById("route").childNodes[0]);
     initialize_route_flag == false;
+    document.getElementById("route").removeChild(document.getElementById("route").childNodes[0]);
   }else{
     initialize_route_flag = true;
   }
